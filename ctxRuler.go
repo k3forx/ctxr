1dcbb56a7b2d091d78cd56ad19d02d822bc64324
Kanata Miyahana <afiri.bupa@gmail.com>
2022-07-31 00:06:15 +0900

    add test

--- ctxRuler.go
+++ ctxRuler.go
@@ -32,10 +32,6 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		(*ast.FuncDecl)(nil),
 	}
 
-	info := &types.Info{
-		Defs: map[*ast.Ident]types.Object{},
-	}
-
 	config := &types.Config{
 		Importer: importer.Default(),
 	}
@@ -43,30 +39,52 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	if err != nil {
 		log.Fatal("context package import error:", err)
 	}
-	ctxType := p.Scope().Lookup("Context").Type()
-	it, ok := ctxType.Underlying().(*types.Interface)
+	it, ok := p.Scope().Lookup("Context").Type().Underlying().(*types.Interface)
 	if !ok {
 		log.Fatal("should be found `Context` interface")
 	}
+	fmt.Printf("it: %+v\n", it)
+	fmt.Printf("pass: %+v\n", pass.TypesInfo.Defs)
+	fmt.Println("-------------------------------------------")
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
 		f, ok := n.(*ast.FuncDecl)
 		if !ok {
 			return
 		}
-		params := f.Type.Params.List
-		for _, p := range params {
-			// ast.Print(fset, p)
+
+		for i, p := range f.Type.Params.List {
 			varIdent := p.Names[0]
 
-			obj := info.ObjectOf(varIdent)
+			obj := pass.TypesInfo.ObjectOf(varIdent)
+			fmt.Printf("i: %+v, obj: %+v\n", i, obj)
 			if obj == nil {
-				return
+				continue
 			}
+			i, ok := obj.Type().Underlying().(*types.Interface)
+			if !ok {
+				continue
+			}
+			fmt.Println(i, it)
+			fmt.Println(i == it)
+			fmt.Println(types.Implements(obj.Type().Underlying(), it))
 
-			if types.Implements(obj.Type(), it) && (varIdent.Name != "ctx") {
-				fmt.Printf("%v: variable name of context.Context is invalid\n", varIdent.Pos())
-			}
+			// tp := pass.TypesInfo.TypeOf(p.Type)
+			// fmt.Printf("f: %+v, i: %+v, p: %+v, var: %+v\n", f.Name, i, p.Type, p.Names[0].Name)
+			// fmt.Printf("tp: %+v, underlying: %+v\n", tp, tp.Underlying())
+
+			// t, ok := tp.Underlying().(*types.Interface)
+			// if !ok {
+			// 	continue
+			// }
+			// fmt.Printf("t : %+v\n", t)
+			// fmt.Printf("it: %+v\n", it)
+
+			// if types.Implements(pass.TypesInfo.TypeOf(p.Type).Underlying(), it) {
+			// 	// fmt.Printf("i: %d, tp name: %s\n", i, p.Names[0].Name)
+			// 	pass.Reportf(f.Pos(), "variable name of `context.Context` is invalid")
+			// }
+			fmt.Println("--------------------")
 		}
 	})
 
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -6,10 +6,10 @@ func f(ctx context.Context, id int) string {
 	return "OK"
 }
 
-func g(c context.Context, id int, email string) string {
+func g(c context.Context, id int, email string) string { // want "variable name of `context.Context` is invalid"
 	return "NG"
 }
 
-func h(id int, email string, ctx context.Context) string {
-	return "NG"
+func h(id int, email string, ctx context.Context) string { // want "variable name of `context.Context` is invalid"
+	return "NG" // want
 }
